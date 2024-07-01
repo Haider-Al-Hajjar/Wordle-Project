c8f5dd139539742048629eba942a56994f7984d9
Haider-Al-Hajjar <alhajjarhaider@gmail.com>
2024-07-01 12:36:54 -0400

    Getting the basic layout all done, and then after that I'll figure out how to get the buttons working. Current objective is to get the base game working, and then the hint system, and then the hard words system, and then the score tracking.

--- pages/Game.jsx
+++ pages/Game.jsx
@@ -7,6 +7,9 @@ function getNewWord() {
     const wordBank = Wordbank()
     return (wordBank)
 }
+function type(letter) {
+    console.log(letter)
+}
 function Game() {
     const navigate = useNavigate()
     const [word, setWord] = useState('')
@@ -19,7 +22,6 @@ function Game() {
                 setDef(newWord[1])
             }}>Get new word</button>
             {word}, {def} */}
-            TEST
             <div className="wordleTable">
                 <button className="b2"></button> <button className="b2"></button> <button className="b2"></button> <button className="b2"></button> <button className="b2"></button> <button className="b2"></button>
                 <br></br>
@@ -34,6 +36,49 @@ function Game() {
                 <br></br>
                 <br></br>
                 <button className="b2"></button> <button className="b2"></button> <button className="b2"></button> <button className="b2"></button> <button className="b2"></button> <button className="b2"></button>
+            </div>
+            <br></br>
+            <br></br>
+            <br></br>
+            <div className="keyboard">
+                <div className="key-row">
+                    <button onClick={() => type("A")}>A</button>
+                    <button onClick={() => type("B")}>B</button>
+                    <button onClick={() => type("C")}>C</button>
+                    <button onClick={() => type("D")}>D</button>
+                    <button onClick={() => type("E")}>E</button>
+                    <button onClick={() => type("F")}>F</button>
+                    <button onClick={() => type("G")}>G</button>
+                    <button onClick={() => type("H")}>H</button>
+                    <button onClick={() => type("I")}>I</button>
+                </div>
+                <br></br>
+                <br></br>
+                <div className="key-row">
+                    <button onClick={() => type("J")}>J</button>
+                    <button onClick={() => type("K")}>K</button>
+                    <button onClick={() => type("L")}>L</button>
+                    <button onClick={() => type("M")}>M</button>
+                    <button onClick={() => type("N")}>N</button>
+                    <button onClick={() => type("O")}>O</button>
+                    <button onClick={() => type("P")}>P</button>
+                    <button onClick={() => type("Q")}>Q</button>
+                    <button onClick={() => type("Enter")}>Ent</button>
+                </div>
+                <br></br>
+                <br></br>
+                <div className="key-row">
+                    <button onClick={() => type("R")}>R</button>
+                    <button onClick={() => type("S")}>S</button>
+                    <button onClick={() => type("T")}>T</button>
+                    <button onClick={() => type("U")}>U</button>
+                    <button onClick={() => type("V")}>V</button>
+                    <button onClick={() => type("W")}>W</button>
+                    <button onClick={() => type("X")}>X</button>
+                    <button onClick={() => type("Y")}>Y</button>
+                    <button onClick={() => type("Z")}>Z</button>
+                    <button onClick={() => type("Backspace")}>Del</button>
+                </div>
             </div>
         </>
     )
